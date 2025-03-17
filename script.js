e997152c1571a7153149af0fb76b45dd259f7f78
samaroho <samarohoc@gmail.com>
2025-03-17 23:52:25 +0530

    scrolling logic added

---
+++ script.js
@@ -0,0 +1,45 @@
+const initialLTRShift = -48 * 4;
+const initialRTLShift = 36 * 4;
+
+function handleNavMenu(){
+    const bars = document.getElementById('bars');
+    const cross = document.getElementById('cross');
+    const navMenuContainer = document.getElementById('nav-menu-container');
+
+    bars.classList.toggle('hidden');
+    cross.classList.toggle('hidden');
+    navMenuContainer.classList.toggle('hidden');
+}
+
+function setUpIntersectionObserver(element, isLTR, speed){
+    const intersectionObserver = new IntersectionObserver(entries => {
+        const isIntersecting = entries[0].isIntersecting;
+
+        if(isIntersecting){
+            document.addEventListener('scroll',scrollHandler);
+        }
+        else{
+            document.removeEventListener('scroll',scrollHandler);
+        }
+    });
+
+    intersectionObserver.observe(element);
+    
+    function scrollHandler(){
+        const shift  = (window.innerHeight - element.getBoundingClientRect().top) * speed;
+        let totalShift;
+
+        if(isLTR){
+            element.style.transform = `translateX(${shift}px)`;
+        }
+        else{
+            element.style.transform = `translateX(${-shift}px)`;
+        }
+
+        
+    }
+}
+
+setUpIntersectionObserver(document.getElementById('line1'), true, 0.15);
+setUpIntersectionObserver(document.getElementById('line2'), false, 0.15);
+setUpIntersectionObserver(document.getElementById('line3'), true, 0.15);
